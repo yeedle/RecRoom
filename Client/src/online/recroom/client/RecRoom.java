4ad07de9e241ab319ced8473e4552ab12932484c
Yeedle <abeneuwirth@gmail.com>
2016-03-22 20:20:29 -0400

    created RecRoom.java

---
+++ Client/src/online/recroom/client/RecRoom.java
@@ -0,0 +1,21 @@
+package online.recroom.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Yeedle on 3/22/2016 8:15 PM.
+ */
+public class RecRoom extends Application
+{
+
+    public static void main(String[] args)
+    {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
