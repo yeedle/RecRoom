7bae788293d2b2c661aec3dc08cf2291f65d5fbb
Yeedle <abeneuwirth@gmail.com>
2016-04-05 22:16:08 -0400

    added methods and annotation for client endpoint

--- Client/src/online/recroom/client/RecRoom.java
+++ Client/src/online/recroom/client/RecRoom.java
@@ -3,12 +3,21 @@ package online.recroom.client;
 import javafx.application. Application;
 import javafx.stage.Stage;
 
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.util.logging.Logger;
+
 /**
- * Created by Yeedle on 3/22/2016 8:15 PM.
+ * This class is both the entry point for our JavaFX client
+ * as well as the webSocket's Client Endpoint.
  */
 
+@ClientEndpoint
 public class RecRoom extends Application
 {
+    private Session session;
+    private static final Logger LOGGER = Logger.getLogger(RecRoom.class.getName());
 
     public static void main(String[] args)
     {
@@ -18,6 +27,36 @@ public class RecRoom extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        connectToWebSoket();
+        //TODO: JavaFX code goes here
+    }
+
+    @OnOpen
+    public void onOpen(Session session)
+    {
+        this.session = session;
+    }
+
+    @OnClose
+    public void onClose(){
+        connectToWebSoket();
+    }
+
+    @OnMessage
+    public void onMessage(String message){
+        //TODO: Create a decoder to handle incoming POJOs.
+    }
+
+    private void connectToWebSoket()
+    {
+
+        WebSocketContainer containter = ContainerProvider.getWebSocketContainer();
+        try {
+            URI uri = URI.create("ws://localhost:8181/games");
+            containter.connectToServer(this, uri);
+        } catch (DeploymentException | IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
