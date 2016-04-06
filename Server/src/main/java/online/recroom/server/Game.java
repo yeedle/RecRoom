459a99aa309baf0d2095da682e44fb202e16d3b0
Yeedle <abeneuwirth@gmail.com>
2016-04-05 21:30:15 -0400

    added a Player Enum and a Game interface

---
+++ Server/src/main/java/online/recroom/server/Game.java
@@ -0,0 +1,9 @@
+package online.recroom.server;
+
+/**
+ * Created by Yeedle on 4/5/2016 8:05 PM.
+ */
+public interface Game {
+
+
+}
---
+++ Server/src/main/java/online/recroom/server/Player.java
@@ -0,0 +1,9 @@
+package online.recroom.server;
+
+/**
+ * Created by Yeedle on 4/5/2016 9:28 PM.
+ */
+public enum Player
+{
+    PLAYER1, PLAYER2;
+}
