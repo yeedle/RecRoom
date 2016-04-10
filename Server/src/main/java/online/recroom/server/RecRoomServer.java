addf00432cee1f03d5bd7df5e8c06e349c03db14
Yeedle <abeneuwirth@gmail.com>
2016-04-10 10:45:20 -0400

    added thread safe hash set to hold all game sessions

--- Server/src/main/java/online/recroom/server/RecRoomServer.java
+++ Server/src/main/java/online/recroom/server/RecRoomServer.java
@@ -6,13 +6,18 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Yeedle on 3/22/2016 8:40 PM.
  */
+
 @ServerEndpoint("/game/{gameId}/{username}")
 public class RecRoomServer
 {
+    protected static Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
 
     @OnOpen
     public void onOpen(Session session, @PathParam("gameId") long gameId,
