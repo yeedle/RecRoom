a36e5f182a3af106c8938f91d8779ef39db68ffa
Yeedle <abeneuwirth@gmail.com>
2016-04-11 21:16:23 -0400

    added a ChessServer class

---
+++ Server/src/main/java/online/recroom/server/chess/ChessServer.java
@@ -0,0 +1,41 @@
+package online.recroom.server.chess;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Created by Yeedle on 4/11/2016 9:07 PM.
+ */
+@ServerEndpoint(
+        value = "/chess/{gameID}/{userID}",
+        decoders = {ChessMoveDecoder.class},
+        encoders = {ChessMoveEncoder.class})
+public class ChessServer
+{
+    private Session session;
+
+
+    @OnOpen
+    public void onOpen(Session session)
+    {
+        this.session = session;
+    }
+
+    @OnMessage
+    public void onMessage(Move move)
+    {
+
+    }
+
+    @OnError
+    public void onError(Throwable throwable)
+    {
+
+    }
+
+    @OnClose
+    public void onClose()
+    {
+
+    }
+}
