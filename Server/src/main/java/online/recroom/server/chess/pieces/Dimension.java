b20ba89a9dd2e4af5689923a403371e7337d07cb
Yeedle <abeneuwirth@gmail.com>
2016-04-28 19:32:15 -0400

    added position class

---
+++ Server/src/main/java/online/recroom/server/chess/pieces/Dimension.java
@@ -0,0 +1,9 @@
+package online.recroom.server.chess.pieces;
+
+/**
+ * Created by Yeedle on 4/28/2016 7:25 PM.
+ */
+public enum Dimension
+{
+    ROW, COLUMN
+}
--- Server/src/main/java/online/recroom/server/chess/pieces/Piece.java
+++ Server/src/main/java/online/recroom/server/chess/pieces/Piece.java
@@ -1,11 +1,19 @@
 package online.recroom.server.chess.pieces;
 
+import java.util.Map;
+
 /**
  * Created by Yeedle on 4/28/2016 7:01 PM.
  */
 public class Piece
 {
-    Position position;
-    Color color;
+    private Map<Dimension, Integer> position;
+    private Color color;
 
+
+    public void setPosition(int row, int column)
+    {
+        position.put(Dimension.ROW, row);
+        position.put(Dimension.COLUMN, column);
+    }
 }
