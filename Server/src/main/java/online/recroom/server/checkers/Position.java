c7c749f4b3d4ee19741e68915b95aa82b46b83b5
Yehuda Globerman <ylgloberman@gmail.com>
2016-05-01 16:51:15 -0400

    Added position class

---
+++ Server/src/main/java/online/recroom/server/checkers/Position.java
@@ -0,0 +1,23 @@
+package online.recroom.server.checkers;
+
+/**
+ * Created by Yehuda Globerman on 5/1/2016.
+ */
+public class Position {
+    private final int row;
+    private final int column;
+
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
