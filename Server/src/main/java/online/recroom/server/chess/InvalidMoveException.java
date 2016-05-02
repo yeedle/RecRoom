a33bbb413a1afad83eb6abd2bb604723ca1b6e63
Yeedle <abeneuwirth@gmail.com>
2016-05-02 19:53:11 -0400

    extended Exception instead of Throwable

--- Server/src/main/java/online/recroom/server/chess/InvalidMoveException.java
+++ Server/src/main/java/online/recroom/server/chess/InvalidMoveException.java
@@ -3,7 +3,7 @@ package online.recroom.server.chess;
 /**
  * Created by Yeedle on 5/2/2016 3:46 PM.
  */
-public class InvalidMoveException extends Throwable
+public class InvalidMoveException extends Exception
 {
 
     public InvalidMoveException()
