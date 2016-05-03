2c37ce2cd7eb282bc5f764380c2f950e229dae7f
Yeedle <abeneuwirth@gmail.com>
2016-05-02 21:29:51 -0400

    refactored isIllegalMove

--- Server/src/main/java/online/recroom/server/chess/pieces/King.java
+++ Server/src/main/java/online/recroom/server/chess/pieces/King.java
@@ -15,7 +15,17 @@ public class King extends Piece
     @Override
     public boolean isIllegalMove(Coordinates origin, Coordinates destination)
     {
-        return (Math.abs(origin.row() - origin.column()) == 1 && origin.column() == destination.column())
-                || (Math.abs(origin.row() - origin.column()) == 1) && origin.row() == destination.row();
+        return isSameColumnOffByOneRow(origin, destination)
+                || isSameRowAndOffByOneColumn(origin, destination);
+    }
+
+    private boolean isSameRowAndOffByOneColumn(Coordinates origin, Coordinates destination)
+    {
+        return (Math.abs(origin.column() - destination.column()) == 1) && origin.row() == destination.row();
+    }
+
+    private boolean isSameColumnOffByOneRow(Coordinates origin, Coordinates destination)
+    {
+        return Math.abs(origin.row() - destination.row()) == 1 && origin.column() == destination.column();
     }
 }
