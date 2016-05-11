228030d0696598f034a5f2de9b62a723a4c47d0b
Yehuda Globerman <yegloberman@yeled.org>
2016-05-11 12:21:43 -0400

    restructure isCaptureMove

--- Server/src/main/java/online/recroom/server/poolCheckers/pieces/BlackPiece.java
+++ Server/src/main/java/online/recroom/server/poolCheckers/pieces/BlackPiece.java
@@ -20,18 +20,16 @@ public class BlackPiece extends Piece implements ICrownable {
 
     @Override
     public boolean isCaptureMove(Cell destination) {
-        if (destination.isOccupied()) {
-            return false;
-        }
-        if (destination.getRow() != (this.getRow() - 2)) {
-            return false;
-        }
-        if (destination.getColumn() == (this.getColumn() + 2)) {
-            return cellInBetweenIsCapturable(getRow() - 1, getColumn() + 1);
-        }
+        if (destination.isNotOccupied() && destination.getRow() == (this.getRow() - 2)) {
+            
+            if (destination.getColumn() == (this.getColumn() + 2)) {
 
-        if (destination.getColumn() == (this.getColumn() - 2)) {
-            return cellInBetweenIsCapturable(getRow() - 1, getColumn() - 1);
+                return cellInBetweenIsCapturable(getRow() - 1, getColumn() + 1);
+
+            } else if (destination.getColumn() == (this.getColumn() - 2)) {
+
+                return cellInBetweenIsCapturable(getRow() - 1, getColumn() - 1);
+            }
         }
         return false;
     }
--- Server/src/main/java/online/recroom/server/poolCheckers/pieces/RedPiece.java
+++ Server/src/main/java/online/recroom/server/poolCheckers/pieces/RedPiece.java
@@ -20,19 +20,18 @@ public class RedPiece extends Piece implements ICrownable {
 
     @Override
     public boolean isCaptureMove(Cell destination) {
-        if (destination.isOccupied()) {
-            return false;
-        }
-        if (destination.getRow() != (this.getRow() + 2)) {
-            return false;
-        }
-        if (destination.getColumn() == (this.getColumn() + 2)) {
-            return cellInBetweenIsCapturable(getRow() + 1, getColumn() + 1);
-        }
+        if (destination.isNotOccupied() && destination.getRow() == (this.getRow() + 2)) {
+            
+            if (destination.getColumn() == (this.getColumn() + 2)) {
 
-        if (destination.getColumn() == (this.getColumn() - 2)) {
-            return cellInBetweenIsCapturable(getRow() + 1, getColumn() - 1);
+                return cellInBetweenIsCapturable(getRow() + 1, getColumn() + 1);
+
+            } else if (destination.getColumn() == (this.getColumn() - 2)) {
+
+                return cellInBetweenIsCapturable(getRow() + 1, getColumn() - 1);
+            }
         }
+        
         return false;
     }
 
