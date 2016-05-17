6f99e80c8331adfdb536d44f7bfb4aa0900ccb4a
Yeedle <abeneuwirth@gmail.com>
2016-05-17 11:09:32 -0400

    added Bubble creation and handling in BubblesEndpoint added TimeLine and KeyFrame Object to BubblesEndpoint

--- Client/src/online/recroom/client/BubblesEndpoint.java
+++ Client/src/online/recroom/client/BubblesEndpoint.java
@@ -1,11 +1,17 @@
 package online.recroom.client;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 import javax.annotation.Resources;
 import javax.websocket.*;
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -21,16 +27,26 @@ public class BubblesEndpoint implements Initializable
     @FXML
     Pane bubblePane;
 
+    File popSoundFile =  new File("src/online/recroom/client/assets/pop.mp3");
+    Media popSound = new Media(popSoundFile.toURI().toString());
+    Timeline t = new Timeline();
+
     @OnOpen
     public void onOpen(final Session session)
     {
 
+        MediaPlayer player = new MediaPlayer(popSound);
     }
 
     @OnMessage
     public void onMessage(final Bubble bubble)
     {
-        this.bubblePane.getChildren().add(bubble);
+
+        bubble.setOnMouseClicked(e -> {bubblePane.getChildren().remove(bubble); new MediaPlayer(popSound).play();});
+
+        t.getKeyFrames().add(new KeyFrame(Duration.millis(40), event1 -> {bubble.move();}));
+        bubblePane.getChildren().add(bubble);
+
     }
 
     @OnError
