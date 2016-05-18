ebc47b8c443ca0320c5cba4b8b4b7efd05c2c9a7
Yeedle <abeneuwirth@gmail.com>
2016-05-18 19:34:11 -0400

    added pieces icons to client

--- Client/src/online/recroom/client/chess/ChessController.java
+++ Client/src/online/recroom/client/chess/ChessController.java
@@ -1,8 +1,41 @@
 package online.recroom.client.chess;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Yeedle on 5/18/2016 4:15 PM.
  */
-public class ChessController
+public class ChessController implements Initializable
 {
+
+    StackPane[][] array = new StackPane[8][8];
+    @FXML
+    GridPane chessBoard;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        int k =0;
+        for (int i =0; i < 8; i++)
+            for (int j=0; j < 8; j++)
+            {
+                array[i][j] = (StackPane) chessBoard.getChildren().get(k++);
+                Image image = new Image(ChessController.class.getResourceAsStream("pieces/white/pawn.png"));
+                array[i][j].getChildren().add(new ImageView(image));
+
+            }
+    }
+
+
 }
--- Client/src/online/recroom/client/chess/ChessIntroController.java
+++ Client/src/online/recroom/client/chess/ChessIntroController.java
@@ -1,9 +1,16 @@
 package online.recroom.client.chess;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -24,6 +31,18 @@ public class ChessIntroController implements Initializable
         System.out.println("hell from init");
 
     }
+
+    @FXML
+    public void newGame(ActionEvent event) throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("chess.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 
 }
