a5c8c479dcc69db03dfce5a5bb389e3b551e06fb
Yeedle <abeneuwirth@gmail.com>
2016-05-18 20:41:50 -0400

    added all the pieces to board in client

--- Client/src/online/recroom/client/chess/ChessController.java
+++ Client/src/online/recroom/client/chess/ChessController.java
@@ -6,10 +6,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,7 +16,7 @@ import java.util.ResourceBundle;
 public class ChessController implements Initializable
 {
 
-    StackPane[][] array = new StackPane[8][8];
+    StackPane[][] pieces = new StackPane[8][8];
     @FXML
     GridPane chessBoard;
 
@@ -30,11 +27,74 @@ public class ChessController implements Initializable
         for (int i =0; i < 8; i++)
             for (int j=0; j < 8; j++)
             {
-                array[i][j] = (StackPane) chessBoard.getChildren().get(k++);
-                Image image = new Image(ChessController.class.getResourceAsStream("pieces/white/pawn.png"));
-                array[i][j].getChildren().add(new ImageView(image));
-
+                pieces[i][j] = (StackPane) chessBoard.getChildren().get(k++);
             }
+
+        positionBishops();
+        positionKings();
+        positionKnights();
+        positionPawns();
+        positionQueens();
+        positionRooks();
+    }
+
+    private void positionQueens()
+    {
+        pieces[3][7].getChildren().add(imageViewOf("pieces/black/queen.png"));
+        pieces[3][0].getChildren().add(imageViewOf("pieces/white/queen.png"));
+    }
+
+    private ImageView imageViewOf(String url)
+    {
+        Image image = new Image(getClass().getResourceAsStream(url));
+       ImageView iv = new ImageView(image);
+        iv.setPreserveRatio(true);
+        iv.setFitWidth(35);
+        iv.setFitHeight(35);
+        return iv;
+
+    }
+
+    private void positionKings()
+    {
+        pieces[4][7].getChildren().add(imageViewOf("pieces/black/king.png"));
+        pieces[4][0].getChildren().add(imageViewOf("pieces/white/pawn.png"));
+    }
+
+    private void positionKnights()
+    {
+        int blackRow =7, whiteRow=0;
+
+        pieces[1][whiteRow].getChildren().add(imageViewOf("pieces/white/knight.png"));
+        pieces[6][whiteRow].getChildren().add(imageViewOf("pieces/white/knight.png"));
+        pieces[1][blackRow].getChildren().add(imageViewOf("pieces/black/knight.png"));
+        pieces[6][blackRow].getChildren().add(imageViewOf("pieces/black/knight.png"));
+
+    }
+
+    private void positionRooks()
+    {
+        pieces[0][7].getChildren().add(imageViewOf("pieces/black/rook.png"));
+        pieces[7][7].getChildren().add(imageViewOf("pieces/black/rook.png"));
+        pieces[0][0].getChildren().add(imageViewOf("pieces/white/rook.png"));
+        pieces[7][0].getChildren().add(imageViewOf("pieces/white/rook.png"));
+    }
+
+    private void positionBishops()
+    {
+        pieces[2][7].getChildren().add(imageViewOf("pieces/black/bishop.png"));
+        pieces[5][7].getChildren().add(imageViewOf("pieces/black/bishop.png"));
+        pieces[2][0].getChildren().add(imageViewOf("pieces/white/bishop.png"));
+        pieces[5][0].getChildren().add(imageViewOf("pieces/white/bishop.png"));
+    }
+
+    private void positionPawns()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            pieces[i][1].getChildren().add(imageViewOf("pieces/white/pawn.png"));
+            pieces[i][6].getChildren().add(imageViewOf("pieces/black/pawn.png"));
+        }
     }
 
 
