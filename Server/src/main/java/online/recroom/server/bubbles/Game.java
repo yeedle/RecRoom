a933b467cc84c02f3e7f250354f3dd75d4f4f20c
Yehuda globerman <ylgloberman@gmail.com>
2016-05-22 15:34:51 -0400

    changed game to account for mutliplayer

--- Server/src/main/java/online/recroom/server/bubbles/Game.java
+++ Server/src/main/java/online/recroom/server/bubbles/Game.java
@@ -2,7 +2,9 @@ package online.recroom.server.bubbles;
 
 import online.recroom.server.Player;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * Created by Yeedle on 5/11/2016 6:25 PM.
@@ -11,13 +13,12 @@ public class Game
 {
     private static long idGenerator = Long.MIN_VALUE;
 
-    public Player player1;
-    public Player player2;
-    public final long id = idGenerator++;
+    private List<BubblePlayer> players = new ArrayList<>();
+    public final long id = idGenerator == -1 ? ++idGenerator : idGenerator++;
     private final HashSet<Bubble> bubbles = new HashSet<>();
 
-    public Game(Player player1) {
-        this.player1 = player1;
+    public Game(BubblePlayer player1) {
+        players.add(player1);
     }
 
     public void generateBubbles(int amount) {
@@ -33,5 +34,13 @@ public class Game
                 break;
             }
         }
+    }
+
+    public void addPlayer(Player p) {
+        players.add(new BubblePlayer(p.name));
+    }
+
+    public void removePlayer(BubblePlayer p) {
+        players.remove(p);
     }
 }
