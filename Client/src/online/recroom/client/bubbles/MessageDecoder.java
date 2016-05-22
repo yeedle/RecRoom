f3a65ab7247bb0cf3796caac0a8656c156576f52
Yeedle <abeneuwirth@gmail.com>
2016-05-22 19:40:08 -0400

    started improving the bubble client

---
+++ Client/src/online/recroom/client/bubbles/MessageDecoder.java
@@ -0,0 +1,39 @@
+package online.recroom.client.bubbles;
+
+import com.google.gson.Gson;
+
+import javax.websocket.DecodeException;
+import javax.websocket.Decoder;
+import javax.websocket.EndpointConfig;
+
+/**
+ * Created by Yehuda Globerman on 5/21/2016.
+ */
+public class MessageDecoder implements Decoder.Text<Message> {
+    private static Gson gson = new Gson();
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public Message decode(String s) throws DecodeException {
+        return gson.fromJson(s, Message.class);
+    }
+
+    @Override
+    public boolean willDecode(String s) {
+        try {
+            gson.fromJson(s, Message.class);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
