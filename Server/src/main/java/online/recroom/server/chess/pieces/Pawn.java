32f6f67fce19cd7e002c25e8191e4ec5ec6aaf5d
Yeedle <abeneuwirth@gmail.com>
2016-05-23 12:28:51 -0400

    finished Pawn legal move checking

--- Server/src/main/java/online/recroom/server/chess/pieces/Pawn.java
+++ Server/src/main/java/online/recroom/server/chess/pieces/Pawn.java
@@ -1,6 +1,8 @@
 package online.recroom.server.chess.pieces;
 
+import com.sun.rowset.internal.Row;
 import online.recroom.server.chess.Board;
+import online.recroom.server.chess.Coordinates;
 import online.recroom.server.chess.Movement;
 
 /**
@@ -13,9 +15,67 @@ public class Pawn extends Piece
         super(player);
     }
 
+    private Coordinates origin;
+    private Coordinates destination;
+
     @Override
     public boolean isLegalMove(Movement move, Board board)
     {
-        return true;
+        origin = move.origin;
+        destination = move.destination;
+
+        return player().equals(Player.BLACK) ? isLegalBlackMove(board) : isLegalWhiteMove(board);
     }
+
+    private boolean isLegalBlackMove(Board board)
+    {
+        if (board.pieceInSquare(destination).isNotEmpty())
+            return movedOneDiagonalDown();
+        return ((inInitialPositionAndMovedTwoDown() || movedOneRowDown()) && stayedInSameColumn());
+    }
+
+    private boolean isLegalWhiteMove(Board board)
+    {
+        if (board.pieceInSquare(destination).isNotEmpty())
+            return movedOneDiagonalUp();
+        return (inInitialPositionAndMovedTwoRowsUp() || movedOneRowUp()) && stayedInSameColumn();
+    }
+
+    private boolean movedOneRowUp()
+    {
+        return destination.row() - origin.row() == 1;
+    }
+
+    private boolean inInitialPositionAndMovedTwoRowsUp()
+    {
+        return origin.row() == 1 && destination.row() - origin.row() == 2;
+    }
+
+    private boolean movedOneDiagonalUp()
+    {
+        return destination.row() - origin.row() == 1;
+    }
+
+
+    private boolean movedOneRowDown()
+    {
+        return origin.row() - destination.row() == 1;
+    }
+
+    private boolean inInitialPositionAndMovedTwoDown()
+    {
+        return origin.row() == 6 && origin.row() - destination.row() == 2;
+    }
+
+    private boolean movedOneDiagonalDown()
+    {
+        return origin.row() - destination.row() == 1 && origin.column() - destination.column() == 1;
+    }
+
+    private boolean stayedInSameColumn()
+    {
+        return origin.column() == destination.column();
+    }
+
+
 }
