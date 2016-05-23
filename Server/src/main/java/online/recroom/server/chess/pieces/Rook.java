3926f2714660501ed6368efb5bc2388cf12c504d
Yeedle <abeneuwirth@gmail.com>
2016-05-23 13:52:12 -0400

    used queen legality checking for rook

--- Server/src/main/java/online/recroom/server/chess/pieces/Rook.java
+++ Server/src/main/java/online/recroom/server/chess/pieces/Rook.java
@@ -1,6 +1,8 @@
 package online.recroom.server.chess.pieces;
 
 import online.recroom.server.chess.Board;
+import online.recroom.server.chess.Coordinates;
+import online.recroom.server.chess.IllegalCoordinateException;
 import online.recroom.server.chess.Movement;
 
 /**
@@ -13,9 +15,63 @@ public class Rook extends Piece
         super(player);
     }
 
+    Board board;
+    Coordinates origin;
+    Coordinates destination;
+    private int hDirection;
+    private int vDirection;
+
     @Override
-    public boolean isLegalMove(Movement move, Board board)
+    public boolean isLegalMove(Movement move, Board board) throws IllegalCoordinateException
     {
-        return false;
+
+        origin = move.origin;
+        destination = move.destination;
+        this.board = board;
+
+        return movedStraight();
+
+    }
+
+    private boolean movedStraight() throws IllegalCoordinateException
+    {
+        return (origin.column()==destination.column() && noPiecesInWayVertically())
+                ||
+                origin.row() == destination.row() && noPiecesInWayHorizontally();
+    }
+
+    private boolean noPiecesInWayVertically() throws IllegalCoordinateException
+    {
+        vDirection = origin.row() < destination.row()? 1 : -1;
+
+        for (int i =1; i < numberOfSquareVerticallyBetweenOriginAndDestination(); i++)
+            if(board.pieceInSquare(removedVerticallyFromOriginBy(i)).isNotEmpty())
+                return false;
+        return true;
+    }
+
+    private boolean noPiecesInWayHorizontally() throws IllegalCoordinateException
+    {
+        hDirection = origin.column() < destination.column()? 1 : -1;
+
+        for (int i =1; i < numberOfSquareHorizontallyBetweenOriginAndDestination(); i++)
+            if(board.pieceInSquare(removedHorizontallyFromOriginBy(i)).isNotEmpty())
+                return false;
+        return true;
+    }
+
+    private int numberOfSquareVerticallyBetweenOriginAndDestination()
+    {
+        return Math.abs(origin.row()-destination.row());
+    }
+    private int numberOfSquareHorizontallyBetweenOriginAndDestination() { return Math.abs(origin.column()-destination.column());}
+
+    private Coordinates removedVerticallyFromOriginBy(int i) throws IllegalCoordinateException
+    {
+        return Coordinates.byColumnAndRow(origin.column(), origin.row()+(i*vDirection));
+    }
+    private Coordinates removedHorizontallyFromOriginBy(int i) throws IllegalCoordinateException
+    {
+        return Coordinates.byColumnAndRow(origin.column()+(i*hDirection), origin.row());
     }
 }
