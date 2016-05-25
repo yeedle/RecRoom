e1eb1f61b2ee96fd570a39d7400c37da8f7342ca
Yeedle <abeneuwirth@gmail.com>
2016-05-25 16:25:17 -0400

    removed unused variables and methods from BubblesController

--- Client/src/online/recroom/client/bubbles/BubblesController.java
+++ Client/src/online/recroom/client/bubbles/BubblesController.java
@@ -28,12 +28,7 @@ public class BubblesController
     @FXML ScrollPane console;
     @FXML VBox vbox;
     BubblesEndpoint endpoint;
-    Session session;
     ConcurrentHashMap<Long, Bubble> bubbleMap = new ConcurrentHashMap<>();
-    File popSoundFile =  new File("Client/src/online/recroom/client/assets/pop.mp3");
-    Media popSound = new Media(popSoundFile.toURI().toString());
-    Timeline t = new Timeline();
-    private final double SPEED = 100;
 
     public void setEndpoint(BubblesEndpoint endpoint)
     {
@@ -74,17 +69,6 @@ public class BubblesController
         Animator.animateBubblePopping(poppedBubbleId, bubble, bubblePane);
         bubbleMap.remove(poppedBubbleId);
     }
-
-    /*private void animateBubblePopping(long poppedBubbleId, Bubble bubble)
-    {
-        ScaleTransition st = new ScaleTransition(Duration.millis(100), bubble);
-        final int  POPPING_SIZE = 5;
-        st.setByX(POPPING_SIZE);
-        st.setByY(POPPING_SIZE);
-        st.setOnFinished(e -> {bubblePane.getChildren().remove(bubble); new MediaPlayer(popSound).play();});
-        st.play();
-
-    }*/
 
     public void console(String str)
     {
