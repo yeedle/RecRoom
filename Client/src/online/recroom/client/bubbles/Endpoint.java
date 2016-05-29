0bec407f7cb040b14c130b075e0b715075369e09
Yeedle <abeneuwirth@gmail.com>
2016-05-28 22:34:39 -0400

    fixed bug in how empty username is treated

--- Client/src/online/recroom/client/bubbles/Endpoint.java
+++ Client/src/online/recroom/client/bubbles/Endpoint.java
@@ -19,7 +19,7 @@ public class Endpoint
     public Endpoint(String username, Controller controller)
     {
         this.username = username.isEmpty()? "Anonymous" : username;
-        WebSocketURI = "ws://localhost:8080/recroom/bubble?username=" + username;
+        WebSocketURI = "ws://localhost:8080/recroom/bubble?username=" + this.username;
         this.controller = controller;
     }
 
