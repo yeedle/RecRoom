d4cd9d759cf3be56dba2b0f0ceab8f0026d7fbd9
Yehuda globerman <ylgloberman@gmail.com>
2016-05-31 23:25:59 -0400

    changed bubble_popped

--- Messages/src/main/java/online/recroom/messages/bubble/enums/BubbleMessages.java
+++ Messages/src/main/java/online/recroom/messages/bubble/enums/BubbleMessages.java
@@ -4,5 +4,5 @@ package online.recroom.messages.bubble.enums;
  * Created by Yehuda Globerman on 5/31/2016.
  */
 public enum BubbleMessages {
-    GAME_PENDING, GAME_STARTED, GAME_OVER, PLAYER_JOINED, PLAYER_LEFT, BUBBLE_POPPED_MESSAGES
+    GAME_PENDING, GAME_STARTED, GAME_OVER, PLAYER_JOINED, PLAYER_LEFT, BUBBLE_POPPED
 }
