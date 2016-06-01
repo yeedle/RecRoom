ed913d685680cee1e241247220230d6e4cbca851
Yehuda globerman <ylgloberman@gmail.com>
2016-05-31 23:16:16 -0400

    changed to enums

--- Messages/src/main/java/online/recroom/messages/Message.java
+++ Messages/src/main/java/online/recroom/messages/Message.java
@@ -1,14 +1,16 @@
 package online.recroom.messages;
 
+import online.recroom.messages.bubble.enums.BubbleMessages;
+
 /**
  * Created by Yeedle on 5/30/2016 5:57 PM.
  */
 public class Message
 {
-    public final Class type;
+    public final BubbleMessages type;
     public final String json;
 
-    public Message(Class type, String json) {
+    public Message(BubbleMessages type, String json) {
         this.type = type;
         this.json = json;
     }
