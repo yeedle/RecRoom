d074aa664086500ed5cc2811d3f32b65041eebdd
Yehuda Globerman <ylgloberman@gmail.com>
2016-06-01 18:55:07 -0400

    FIXED MESSAGED SCHEME!!!

--- Messages/src/main/java/online/recroom/messages/MessageDecoder.java
+++ Messages/src/main/java/online/recroom/messages/MessageDecoder.java
@@ -1,6 +1,7 @@
 package online.recroom.messages;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 import javax.websocket.DecodeException;
 import javax.websocket.Decoder;
@@ -14,12 +15,19 @@ public class MessageDecoder implements Decoder.Text<Message>
     private static Gson gson = new Gson();
     public Message decode(String s) throws DecodeException
     {
+        if (!willDecode(s))
+            throw new JsonSyntaxException("Garbage data");
         return gson.fromJson(s, Message.class);
     }
 
     public boolean willDecode(String s)
     {
-        return false;
+        try {
+            gson.fromJson(s, Message.class);
+            return true;
+        } catch (JsonSyntaxException e) {
+            return false;
+        }
     }
 
     public void init(EndpointConfig config)
